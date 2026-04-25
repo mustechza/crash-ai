f521998ab271e7befffc929f55c32db51aba5bdd
mustechza <141557155+mustechza@users.noreply.github.com>
2026-04-25 10:57:29 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,1 +1,26 @@
 
+def parse(doc):
+    try:
+        d = doc.get("document")
+        if not d:
+            return None
+
+        f = d.get("fields", {})
+
+        def get(v):
+            if "doubleValue" in v:
+                return float(v["doubleValue"])
+            if "integerValue" in v:
+                return int(v["integerValue"])
+            if "stringValue" in v:
+                return v["stringValue"]
+            return None
+
+        return {
+            "id": d["name"].split("/")[-1],
+            "multiplier": get(f.get("multiplier", {})),
+            "timestamp": get(f.get("timestamp", {}))
+        }
+
+    except:
+        return None
